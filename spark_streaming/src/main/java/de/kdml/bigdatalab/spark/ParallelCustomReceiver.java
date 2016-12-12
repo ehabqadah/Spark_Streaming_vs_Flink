942a72edcca1597bbeafaddae0be5372c09c1bb5
Ehab <ehabqadah@gmail.com>
2016-12-12 23:25:53 +0100

    added parallel custom receiver

--- spark_streaming/src/main/java/de/kdml/bigdatalab/spark/ParallelCustomReceiver.java
+++ spark_streaming/src/main/java/de/kdml/bigdatalab/spark/ParallelCustomReceiver.java
@@ -19,17 +19,20 @@ import org.apache.spark.streaming.receiver.Receiver;
 
 import com.google.common.io.Closeables;
 
+import de.kdml.bigdatalab.spark.NetworkCustomReceiver.CustomStreamReceiver;
 import scala.Tuple2;
 
 /**
- * his a example of the Spark stream custom receiver with parallel receiving
- * data from two different sockets
+ * his a example of the Spark stream custom receiver 
+ * Please refer to {@linkplain https://spark.apache.org/docs/2.0.2/streaming-custom-receivers.html#spark-streaming-custom-receivers}
+ 
+ * @author Ehab Qadah 
  * 
- * @author Ehab Qadah
- * 
- *         Dec 10, 2016
+ * Dec 10, 2016
  */
-public class NetworkCustomReceiver {
+public class ParallelCustomReceiver {
+
+
 
 	private static Configs configs = Configs.getInstance();
 
@@ -38,13 +41,13 @@ public class NetworkCustomReceiver {
 		JavaSparkContext sc = SparkConfigsUtils.getSparkContext("CustomReceiver");
 		JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.seconds(configs.getIntProp("batchDuration")));
 
-		// Create an input stream with the custom receiver on host:port1
-
+		// Create an input stream with the custom receiver on host1:port1 and host2:port2
 		CustomStreamReceiver customStreamReceiver = new CustomStreamReceiver(configs.getStringProp("socketHost"),
-				configs.getIntProp("socketPort"));
+				configs.getIntProp("socketPort"),configs.getStringProp("socketHost"),configs.getIntProp("socketPort")+1);
 
 		JavaReceiverInputDStream<String> lines = ssc.receiverStream(customStreamReceiver);
 
+		
 		JavaPairDStream<String, Integer> wordCounts = lines.flatMapToPair(line -> {
 
 			List<Tuple2<String, Integer>> tuples = new ArrayList<>();
@@ -74,15 +77,16 @@ public class NetworkCustomReceiver {
 	 */
 	public static class CustomStreamReceiver extends Receiver<String> {
 
-		private static long count = 0;
 		private static final long serialVersionUID = 1L;
-		String host = null;
-		int port = -1;
+		private String host = null, host2 = null;
+		private int port = -1, port2 = -1;
 
-		public CustomStreamReceiver(String host, int port) {
+		public CustomStreamReceiver(String host, int port, String host2, int port2) {
 			super(StorageLevel.MEMORY_AND_DISK_2());
 			this.host = host;
 			this.port = port;
+			this.host2 = host2;
+			this.port2 = port2;
 		}
 
 		public void onStart() {
@@ -90,18 +94,30 @@ public class NetworkCustomReceiver {
 			new Thread() {
 				@Override
 				public void run() {
-					receive();
+					receive(CustomStreamReceiver.this.host, CustomStreamReceiver.this.port);
+					
 				}
 			}.start();
-		}
+		
+		
+		new Thread() {
+			@Override
+			public void run() {
+				receive(CustomStreamReceiver.this.host2, CustomStreamReceiver.this.port2);
+				
+			}
+		}.start();
+	}
 
 		public void onStop() {
+			// There is nothing much to do as the thread calling receive()
+			// is designed to stop by itself isStopped() returns false
 		}
 
 		/**
 		 * Create a socket connection and receive data until receiver is stopped
 		 */
-		private void receive() {
+		private void receive(String host, int port) {
 			try {
 				Socket socket = null;
 				BufferedReader reader = null;
@@ -112,7 +128,7 @@ public class NetworkCustomReceiver {
 					reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
 					// Until stopped or connection broken continue reading
 					while (!isStopped() && (userInput = reader.readLine()) != null) {
-						System.out.println(++count + " > Received data '" + userInput + "'");
+						System.out.println(" Received data '" + userInput + "' from " + host + ":" + port);
 						store(userInput);
 					}
 				} finally {
