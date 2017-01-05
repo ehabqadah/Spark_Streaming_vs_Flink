0706f477f4b8329108e7a0c4957dc343d27e193a
ehab <ehabqadah@gmail.com>
2017-01-05 15:40:27 +0100

    Added sector(polygon) utils

---
+++ common_utils/src/main/java/de/kdml/bigdatalab/spark_and_flink/common_utils/SectorUtils.java
@@ -0,0 +1,59 @@
+package de.kdml.bigdatalab.spark_and_flink.common_utils;
+
+import org.geotools.geometry.jts.JTSFactoryFinder;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+
+/**
+ * 
+ * @author Ehab Qadah
+ * 
+ *         Jan 5, 2017
+ */
+public class SectorUtils {
+
+	public static GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory(null);
+
+	/**
+	 * Get Polygon Well-Known Text format
+	 * 
+	 * @param polygonWKT
+	 * @return
+	 */
+	public static Polygon getPolygon(String polygonWKT) {
+
+		Polygon polygon = null;
+
+		WKTReader reader = new WKTReader(geometryFactory);
+		try {
+			polygon = (Polygon) reader.read(polygonWKT);
+		} catch (ParseException e) {
+			System.out.println("unable to parse polygon" + e.getMessage());
+			return null;
+		}
+
+		return polygon;
+
+	}
+
+	/**
+	 * Check if the point in the polygon
+	 * 
+	 * @param polygon
+	 * @param longtitude
+	 * @param latitude
+	 * @return
+	 */
+	public static boolean isPointInPolygon(Polygon polygon, double longtitude, double latitude) {
+
+		Coordinate coord = new Coordinate(-2.933349609375, 55.24155203565249);
+		Point point = geometryFactory.createPoint(coord);
+
+		return polygon.intersection(point).equals(point);
+	}
+}
