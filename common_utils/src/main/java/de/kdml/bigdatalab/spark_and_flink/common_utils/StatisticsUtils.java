d2a040e9c1ec9cb8a98906e1a33289cac3823d92
ehab <ehabqadah@gmail.com>
2017-01-24 22:27:24 +0100

    Added method to calculate statistics  between two consecutive trajectories

--- common_utils/src/main/java/de/kdml/bigdatalab/spark_and_flink/common_utils/StatisticsUtils.java
+++ common_utils/src/main/java/de/kdml/bigdatalab/spark_and_flink/common_utils/StatisticsUtils.java
@@ -1,9 +1,6 @@
 package de.kdml.bigdatalab.spark_and_flink.common_utils;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 import de.kdml.bigdatalab.spark_and_flink.common_utils.data.Trajectory;
 import de.kdml.bigdatalab.spark_and_flink.common_utils.data.TrajectoryStatisticsWrapper;
@@ -79,6 +76,79 @@ public class StatisticsUtils {
 		}
 
 		return trajectories;
+	}
+
+	/**
+	 * Compute statistics by aggregating from old trajectory
+	 * 
+	 * @param oldTrajectory
+	 * @param newTrajectory
+	 */
+	public static void computeStatistics(Trajectory oldTrajectory, Trajectory newTrajectory) {
+
+		intitStaticsForOldTrajectory(oldTrajectory);// initialize statics for
+													// the first trajectory only
+		double minLongtitude, minLatitude, minAltitude, minSpeed, minAcceleration;
+		double maxLongtitude, maxLatitude, maxAltitude, maxSpeed, maxAcceleration;
+		TrajectoriesUtils.calculateDistanceAndSpeedOfTrajectory(oldTrajectory, newTrajectory);
+		double longtitude = newTrajectory.getLongitude(), lat = newTrajectory.getLatitude(),
+				altit = newTrajectory.getAltitude(), speed = newTrajectory.getSpeed(),
+				acceleration = newTrajectory.getAcceleration();
+		TrajectoryStatisticsWrapper oldStatistics = oldTrajectory.getStatistics();
+		// update min longitude
+		minLongtitude = Math.min(oldStatistics.getMinLong(), longtitude);
+		// update min latitude
+		minLatitude = Math.min(oldStatistics.getMinLat(), lat);
+		minAltitude = Math.min(oldStatistics.getMinAltitude(), altit);
+		minAcceleration = Math.min(oldStatistics.getMaxAcceleration(), acceleration);
+		minSpeed = Math.min(oldStatistics.getMinSpeed(), speed);
+		// update max longitude
+		maxLongtitude = Math.max(oldStatistics.getMaxLong(), longtitude);
+		// update max latitude
+		maxLatitude = Math.max(oldStatistics.getMaxLat(), lat);
+		maxAltitude = Math.max(oldStatistics.getMaxAltitude(), altit);
+		maxSpeed = Math.max(oldStatistics.getMaxSpeed(), speed);
+		maxAcceleration = Math.max(oldStatistics.getMaxAcceleration(), acceleration);
+
+		TrajectoryStatisticsWrapper statistics = new TrajectoryStatisticsWrapper();
+		statistics.setMinLong(minLongtitude);
+		statistics.setMinLat(minLatitude);
+		statistics.setMinAltitude(minAltitude);
+		statistics.setMaxLong(maxLongtitude);
+		statistics.setMaxLat(maxLatitude);
+		statistics.setMaxAltitude(maxAltitude);
+		statistics.setMaxAcceleration(maxAcceleration);
+		statistics.setMinAcceleration(minAcceleration);
+		statistics.setMaxSpeed(maxSpeed);
+		statistics.setMinSpeed(minSpeed);
+
+		newTrajectory.setStatistics(statistics);
+	}
+
+	/**
+	 * Initialize the statistics of the first trajectory
+	 * 
+	 * @param oldTrajectory
+	 */
+	private static void intitStaticsForOldTrajectory(Trajectory oldTrajectory) {
+		if (oldTrajectory.isNew() || oldTrajectory.getStatistics() == null) {
+
+			TrajectoryStatisticsWrapper statistics = new TrajectoryStatisticsWrapper();
+			statistics.setMinLong(oldTrajectory.getLongitude());
+			statistics.setMinLat(oldTrajectory.getLatitude());
+			statistics.setMinAltitude(oldTrajectory.getAltitude());
+			statistics.setMaxLong(oldTrajectory.getLongitude());
+			statistics.setMaxLat(oldTrajectory.getLatitude());
+			statistics.setMaxAltitude(oldTrajectory.getAltitude());
+			statistics.setMaxAcceleration(0);
+			statistics.setMinAcceleration(0);
+			statistics.setMaxSpeed(0);
+			statistics.setMinSpeed(0);
+
+			oldTrajectory.setStatistics(statistics);
+
+		}
+
 	}
 
 }
