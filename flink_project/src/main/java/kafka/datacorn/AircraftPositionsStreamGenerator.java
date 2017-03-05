2973c8b1c26b23e013636147b1a3e5b769d62a04
ehab <ehabqadah@gmail.com>
2017-03-06 00:02:34 +0100

    updated ADS-B messages producer

---
+++ flink_project/src/main/java/kafka/datacorn/AircraftPositionsStreamGenerator.java
@@ -0,0 +1,61 @@
+package kafka.datacorn;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+import de.kdml.bigdatalab.spark_and_flink.common_utils.Configs;
+import de.kdml.bigdatalab.spark_and_flink.common_utils.data.StreamRecord;
+
+/**
+ * ADS-B messages stream source
+ * 
+ * @author Ehab Qadah
+ * 
+ *         Mar 5, 2017
+ */
+public class AircraftPositionsStreamGenerator implements SourceFunction<String> {
+
+	private static final int LINE_SLIDE_TIME_MS = 0;
+
+	private static final long serialVersionUID = 2174904787118597072L;
+	private static Configs configs = Configs.getInstance();
+	boolean running = true;
+	int i = 0;
+
+	@Override
+	public void run(SourceContext<String> ctx) throws Exception {
+
+		while (running) {
+
+			String[] adsbFiles = configs.getStringProp("adsbFiles").split(",");
+			for (String file : adsbFiles) {
+
+				System.out.println("streaming of " + file);
+				i++;
+				try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+					String messageLine;
+					while ((messageLine = br.readLine()) != null) {
+
+						// append streaming time
+						messageLine = new StreamRecord(messageLine).toString();
+						ctx.collect(messageLine);
+						// Thread.sleep(LINE_SLIDE_TIME_MS);
+					}
+				} catch (Exception e) {
+
+					System.out.println("exp" + e);
+				}
+			}
+			// running = false;
+
+		}
+	}
+
+	@Override
+	public void cancel() {
+		running = false;
+	}
+
+}
--- flink_project/src/main/java/kafka/datacorn/DatacornKafkaStreamProducer.java
+++ flink_project/src/main/java/kafka/datacorn/DatacornKafkaStreamProducer.java
@@ -1,7 +1,5 @@
 package kafka.datacorn;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.Serializable;
 import java.util.Properties;
 
@@ -9,17 +7,15 @@ import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer09;
 import org.apache.flink.streaming.connectors.kafka.partitioner.KafkaPartitioner;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
 import de.kdml.bigdatalab.spark_and_flink.common_utils.Configs;
-import de.kdml.bigdatalab.spark_and_flink.common_utils.data.StreamRecord;
 
 /**
- * This kafka's stream of lines producer using FlinkKafkaProducer09 it writes a
- * new random line every 1 second
+ * This kafka's stream of lines producer using FlinkKafkaProducer09 it writes
+ * the ADS-B messages
  * 
  * @author Ehab Qadah
  * 
@@ -39,18 +35,18 @@ public class DatacornKafkaStreamProducer {
 		env.setParallelism(4);
 
 		// add a simple source which is writing some strings
-		DataStream<String> messageStream = env.addSource(new SimpleStringGenerator());
+		DataStream<String> messageStream = env.addSource(new AircraftPositionsStreamGenerator());
 
 		Properties producerConfig = new Properties();
 		producerConfig.put("bootstrap.servers", configs.getStringProp("bootstrap.servers"));
 		// // write stream to Kafka
 		messageStream.addSink(new FlinkKafkaProducer09<String>(configs.getStringProp("topicId"),
-				new SimpleStringSchema(), producerConfig, new TestPartitioner()));
+				new SimpleStringSchema(), producerConfig, new RandomPartitioner()));
 
 		env.execute("kafka stream ");
 	}
 
-	public static class TestPartitioner extends KafkaPartitioner<String> implements Serializable {
+	public static class RandomPartitioner extends KafkaPartitioner<String> implements Serializable {
 		private static final long serialVersionUID = 1627268846962918126L;
 
 		private int targetPartition = -1;
@@ -69,52 +65,11 @@ public class DatacornKafkaStreamProducer {
 		@Override
 		public int partition(String next, byte[] serializedKey, byte[] serializedValue, int numPartitions) {
 			if (targetPartition >= 0) {
-				// System.out.println("part="+targetPartition +" next="+next +"
-				// value="+new String(serializedValue) );
 				return targetPartition;
 			} else {
 				throw new RuntimeException("The partitioner has not been initialized properly");
 			}
 		}
-	}
-
-	public static class SimpleStringGenerator implements SourceFunction<String> {
-		/**
-		 * Generates a new random line every LINE_SLIDE_TIME_MS
-		 */
-		private static final int LINE_SLIDE_TIME_MS = 2 * 1000;
-
-		private static final long serialVersionUID = 2174904787118597072L;
-
-		boolean running = true;
-		int i = 0;
-
-		@Override
-		public void run(SourceContext<String> ctx) throws Exception {
-			while (running) {
-				i++;
-				try (BufferedReader br = new BufferedReader(new FileReader("../data/ADSBHUB.1439503200353"))) {
-					String line;
-					while ((line = br.readLine()) != null) {
-
-						line = new StreamRecord(line).toString();
-						ctx.collect(line);
-						Thread.sleep(LINE_SLIDE_TIME_MS);
-					}
-				} catch (Exception e) {
-
-					System.out.println("exp" + e);
-				}
-				running = false;
-
-			}
-		}
-
-		@Override
-		public void cancel() {
-			running = false;
-		}
-
 	}
 
 }
